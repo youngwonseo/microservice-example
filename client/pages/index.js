a222f07d7c2973c5752ef38327003c6059d9c05f
youngwonseo <jazz9008@gmail.com>
2021-05-01 17:18:24 +0900

    client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,41 @@
-import axios from 'axios';
+import Link from 'next/link';
 
-const LandingPage = ({ color }) => {
-  return <h1>Lading Page1</h1>
-};
-
-LandingPage.getInitialProps = ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // we are on the server
-    // requests should be made to http://ingress-nginx.ingress-nginx.dev
-    // 'http://SERVICENAME.NAMESPACE.svc.cluster.local'
-    const { data } = await axios.get(
-      'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: {
-          Host: req.headers
-        }
-      }
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
     );
+  });
 
-
-    return data;
-  } else {
-    // we are on the browser
-    const { data } = await axios.get('/api/users/currentuser');
-    // { currentUser: }
-    return data;
-  }
-
-
-  return {};
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
+};
 
 export default LandingPage;
